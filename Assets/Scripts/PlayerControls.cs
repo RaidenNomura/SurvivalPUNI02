90e2bf8df924b5d9f22ae8535f5e62c6ad72c2fb
FishyTree <53624883+camcartier@users.noreply.github.com>
2023-02-10 14:35:05 +0100

    update playerPrefab (suppression dans le dosser character)

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    private Animator _animator;
+    private Rigidbody _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
